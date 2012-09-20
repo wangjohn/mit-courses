4e4ea0ba53ea6526123bc6c5b315034362f5cc83
John J Wang <wangjohn@mit.edu>
2012-09-19 23:07:50 -0400

    Completed 6.004 pset 1.

--- 6.034-labs/lab0/key.py
+++
@@ -1,5 +0,0 @@
-# Login information for the 6.034 Automated Tester
-
-USERNAME="wangjohn_MIT_EDU"
-PASSWORD="4PQQB67VJZvPkDAFr4xG"
-XMLRPC_URL="https://6.034.scripts.mit.edu:444/fall12/tester/xmlrpc/"
