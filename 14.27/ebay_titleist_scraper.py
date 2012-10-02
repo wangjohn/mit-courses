58b80c181c79ece86bcf826bf811c06e068eaf45
John <john@ubuntu.(none)>
2012-10-01 22:42:01 -0400

    Working on problem 3

--- 14.27/ebay_titleist_scraper.py
+++ 14.27/ebay_titleist_scraper.py
@@ -9,7 +9,9 @@ def find_result_set_items(url):
     soup = BeautifulSoup(page)
     
     results = []
+    counter = 0
     for item in soup.find('div', {'id':'ResultSetItems'}).findAll('table', {'class': 'li rsittlref'}):
+        counter += 1
         # get the soup objects
         title_link_soup = item.find('div', {'class': 'ittl'})
         price_soup = item.find('td', {'class': 'prc'})
@@ -34,9 +36,8 @@ def find_result_set_items(url):
             unit_price = None
 
         # get the item link
-        examine_item(item.find('div', {'class':'ittl'}).a["href"])
-
-        results.append({
+        item_bid_results = examine_item(item.find('div', {'class':'ittl'}).a["href"])
+        next_result_hash = {
             'title': title,
             'num_dozens': num_dozens,
             'price': price, 
@@ -45,11 +46,16 @@ def find_result_set_items(url):
             'unit_price': unit_price,
             'percentage_capitals': percentage_capitals,
             'number_annoying_punctuation': number_exclamations
-        })
+        }
+        for key, value in item_bid_results.iteritems():
+            next_result_hash[key] = value
+        results.append(next_result_hash)
+        print "Result %s: next_result_hash['title']" % str(counter)
     return results
 
 def get_title(title_link_soup):
-    return str(title_link_soup.find('a', text=True))
+    title = str(title_link_soup.find('a', text=True))
+    return re.sub(";","-",title) # replace semicolons so it doesn't goof up csv formatting
 
 def get_price(price_soup):
     regex_search = re.search("<div class=\"(.*?)(bidsold|binsold)\".*?>\s*\$(.*?)</div>", str(price_soup))
@@ -66,8 +72,14 @@ def get_sold_status(num_bids_soup):
         return True
     return False
 
+# returns a hash results of the following spec:
+# results["seller_score"] is the seller's ebay score
+# results["buyers"] is a list of lists for each bid in the bid histry
+#   [buyer_score, price of bid, date of bid]
+# results["bid_history_link"] is a link leading to bid history
+# results["auction_link"] is a link leading to the auction page
 def examine_item(link):
-    results = {}
+    results = {"auction_link": link}
     try:
         page = urllib2.urlopen(link)
     except:
@@ -82,12 +94,11 @@ def examine_item(link):
             results["seller_score"] = search_result.group(1)
             break
 
-    print results["seller_score"]
     bid_history_soup = soup.find("div", {"class": "vi-cvip-bidt1"})
     if bid_history_soup:
         # If there is bid history, get the list of buyers and their bids
         bid_history_link = bid_history_soup.a["href"]
-        print bid_history_link
+        results["bid_history_link"] = bid_history_link
         try:
             bid_history_page = urllib2.urlopen(bid_history_link)
             bid_history_soup = BeautifulSoup(bid_history_page)
@@ -95,18 +106,38 @@ def examine_item(link):
             return results
         history_table = bid_history_soup.find("div", {"id": "vizrefdiv"})
         buyers = history_table.findAll("tr")
+        results["buyers"] = []
         for i in xrange(len(buyers)):
-            buyer = buyers[i]
-            buyer_score = buyer.find("a", {"id":"feedBackScoreDiv3"}, text=True)
-            if buyer_score:
-                results["buyers"].append([buyer_score])
-            bid_info = buyer.findAll("td", {"class":"contentValueFont"})
-            for i in xrange(len(bid_info)//2):
-                cost = bid_info[i].find("span", text=True)
-                datetime = bid_info[i+1].findAll("span", text=True)
-                print cost, datetime
-        print bid_info
-        print results["buyers"] 
+            # skip the first row, since it is just the header
+            if i > 0:
+                buyer = buyers[i]
+                # get the buyer's ebay reputation score
+                search_result = re.search("<a id=\"feedBackScoreDiv3\">(.*?)</a>", str(buyer))
+                try:
+                    buyer_score = search_result.group(1)
+                except:
+                    buyer_score = None
+                bid_info = buyer.findAll("td", {"class":"contentValueFont"})
+                cost = None
+                date = None
+                # get the info from the contentValueFont class
+                if bid_info:
+                    search_result = re.search("US\s*\$(.*?)</span>", str(bid_info[0]))
+                    try:
+                        cost = search_result.group(1)
+                    except:
+                        cost = None 
+                    date = bid_info[1].findAll("span", text=True)
+                    try:
+                        date = [str(result).strip() for result in date]
+                        date = datetime.strptime(' '.join(date).strip(), "%b-%d-%y %H:%M:%S PDT")
+                    except:
+                        date = None
+
+                # add the information to the results hash, if we have any 
+                if buyer_score or cost or date:
+                    results["buyers"].append([buyer_score, cost, date])
+    return results
 
 def get_time(time_soup):
     if time_soup:
@@ -183,14 +214,23 @@ def get_number_exclamations(title):
 def print_to_csv(results, filename):
     with open(filename, 'wb') as f:
         writer = csv.writer(f)
-        item_list = ['title','num_dozens','price','was_sold','date','unit_price','percentage_capitals','number_annoying_punctuation']
-        writer.writerow(item_list)
+        item_list = ['title','num_dozens','price','was_sold','date','unit_price','percentage_capitals','number_annoying_punctuation','seller_score','auction_link','bid_history_link']
+        extra_items = ['buyer_score', 'price_of_bid', 'date_of_bid', 'final_buyer']
+        writer.writerow(item_list + extra_items)
         for i in xrange(len(results)):
-            writer.writerow([results[i][item] for item in item_list])
+            if "buyers" in results[i]:
+                for j in xrange(len(results[i]["buyers"])):
+                    buyer_bid = results[i]["buyers"][j]
+                    buyer_bid.append(True if j == 0 else False)
+                    writer.writerow([(results[i][item] if (item in results[i]) else None) for item in item_list] + buyer_bid)
+            else:
+                writer.writerow([(results[i][item] if (item in results[i]) else None) for item in item_list])
 
 if __name__ == '__main__':
     results = []
     #results.extend(find_result_set_items('http://www.ebay.com/sch/i.html?_sadis=200&LH_ItemCondition=3&LH_SALE_CURRENCY=0&_from=R40&_sacat=0&_adv=1%7C1&_sop=13&_dmd=1&LH_Complete=1&_nkw=titleist+prov1+golf+balls+new+1+dozen&rt=nc'))
     #results.extend(find_result_set_items('http://www.ebay.com/sch/i.html?_sadis=200&_adv=1%7C1&LH_ItemCondition=3&_sop=13&LH_SALE_CURRENCY=0&_sacat=0&_from=R40&LH_Complete=1&_dmd=1&_nkw=titleist+prov1+golf+balls+new+1+dozen&_pgn=2&_skc=200&rt=nc'))
-    results.extend(find_result_set_items('http://www.ebay.com/sch/i.html?_sadis=200&LH_ItemCondition=3&LH_SALE_CURRENCY=0&_from=R40&_sacat=0&_adv=1%7C1&_sop=13&_dmd=1&LH_Complete=1&_nkw=titleist+prov1+golf+balls+new+1+dozen&_pgn=3&_skc=400&rt=nc'))
-    #print_to_csv(results, 'ps2_scraped_data_2.csv')
+    #results.extend(find_result_set_items('http://www.ebay.com/sch/i.html?_sadis=200&LH_ItemCondition=3&LH_SALE_CURRENCY=0&_from=R40&_sacat=0&_adv=1%7C1&_sop=13&_dmd=1&LH_Complete=1&_nkw=titleist+prov1+golf+balls+new+1+dozen&_pgn=3&_skc=400&rt=nc'))
+    results.extend(find_result_set_items("http://www.ebay.com/sch/i.html?_nkw=callaway+tour+golf+balls+dozen&_sacat=0&_odkw=callaway+tour+golf+balls+1+dozen&_sadis=200&_adv=1%7C1&_sop=13&LH_SALE_CURRENCY=0&_osacat=0&_from=R40&_dmd=1&LH_Complete=1"))
+    results.extend(find_result_set_items('http://www.ebay.com/sch/i.html?_sadis=200&_adv=1%7C1&_sop=13&LH_SALE_CURRENCY=0&_sacat=0&_from=R40&LH_Complete=1&_dmd=1&_nkw=callaway+tour+golf+balls+dozen&_pgn=2&_skc=200&rt=nc'))
+    print_to_csv(results, 'ps2_scraped_data_callway.csv')
--- 14.27/ps2_r_scripts.r
+++ 14.27/ps2_r_scripts.r
@@ -1,6 +1,23 @@
-data = read.csv("ps2_scraped_data_2.csv", header=TRUE), sep=";")
+#data = read.csv("ps2_scraped_data_2.csv", header=TRUE, sep=";")
+#
+#hist(data$unit_price, main="Titleist ProV1 Golf Balls", xlab="Price Per Dozen")
+#dev.copy(png, 'unit_price_histogram.png')
+#dev.off()
 
-hist(data$unit_price, main="Titleist ProV1 Golf Balls", xlab="Price Per Dozen")
-dev.copy(png, 'unit_price_histogram.png')
-dev.off()
+data = read.csv("ps2_scraped_data_3.csv", header=TRUE, sep=";")
+data$was_sold = as.numeric(data$was_sold)-1
+final_buyer_data = data[data$final_buyer=='True',] 
 
+# regressions to see how price varies with buyer reputation
+result_base = glm(unit_price ~ buyer_score + seller_score, data=final_buyer_data)
+result_2 = glm(unit_price ~ buyer_score + seller_score + percentage_capitals + number_annoying_punctuation, data=final_buyer_data)
+
+# regressions to see how these variables affect how close you were to the actual sale price
+data$distance_to_final_price = data$price - data$price_of_bid
+result_3 = glm(distance_to_final_price ~ buyer_score + seller_score, data=data)
+
+summary(result_base)
+summary(result_2)
+summary(result_3)
+
+    
