6c4999601d68f08755c96cb117272bf2926a4a78
John J Wang <wangjohn@mit.edu>
2012-11-07 23:41:37 -0500

    Working on 6.02 lab.

--- 6.02/PS6_audiocom/demodulate.py
+++ 6.02/PS6_audiocom/demodulate.py
@@ -4,22 +4,27 @@ import sendrecv
 import math
 
 def avgfilter(samples_in, carrier_freq, config):
+    window = config.spb / 2 
     y = [0]*len(samples_in)
     for i in range(len(y)):
         y[i] = numpy.mean(samples_in[i:i+window])
     return numpy.array(y)
 
 def lpfilter(samples_in, carrier_freq, config):
-    raise NotImplementedError, 'lpfilter'
+    cutoff_freq = (config.changap*config.spb/(2*config.samplerate))
+    h = [math.sin(2*math.pi*cutoff_freq*i)/(i*math.pi) for i in xrange(-50, 51, 1)]
+    h = numpy.array(h)
+    return numpy.convolve(samples_in, h)
 
 def envelope(samples, carrier_freq, config):
+    sample_rate = config.samplerate
     return avgfilter(numpy.abs(samples), (sample_rate/carrier_freq)/2)
 
 def heterodyne(samples, carrier_freq, config):
-    print config
-    sample_rate = 0
-    sendrecv.local_carrier(carrier_freq, len(samples), sample_rate)
-    raise NotImplementedError, 'heterodyne'
+    sample_rate = config.samplerate
+    local_carrier = sendrecv.local_carrier(carrier_freq, len(samples), sample_rate)
+    return numpy.multiply(samples, local_carrier)
 
 def quadrature(samples, carrier_freq, config):
-    raise NotImplementedError, 'quadrature'
+    quadcarrier = sendrecv.local_carrier(carrier_freq, len(samples), config.samplerate, "demodquad")
+    return numpy.multiply(samples, quadcarrier)
