c8fa92a66ddf5790d3bd0c9c189b0827043840e2
John J Wang <wangjohn@mit.edu>
2012-11-14 16:56:36 -0500

    Commented a significant portion of the writeup.

--- interviews/knewton/examset.py
+++ interviews/knewton/examset.py
@@ -1,10 +1,13 @@
 class QuestionResult:
+    """Class for storing the result of a question. Used when reading in data."""
     def __init__(self, question_id, student_id, correct):
         self.question_id = question_id
         self.student_id = student_id
         self.correct = correct
 
 class StudentResult:
+    """Class for storing the result of a particular student's performance on 
+       test exams. Created and populated when reading in data an processing it."""
     def __init__(self, question_results, student_id, num_correct, num_questions):
         self.question_results = question_results
         self.student_id = student_id
@@ -16,18 +19,23 @@ class StudentResult:
         return self.num_correct*1.0 / self.num_questions
 
 class Question:
+    """Class for one of the questions asked. Methods provide a question_id, 
+       a probability of getting the question right, and the entropy."""
     def __init__(self, question_id, rj):
         self.question_id = question_id
         self.rj = rj
         self.entropy = get_entropy(self.rj) + get_entropy(1 - self.rj) 
 
 class Student:
+    """Potential student. This is a model of a random student taking a set of
+       k questions. Used when creating an ExamSet and evaluating the set."""
     def __init__(self, questions):
         self.questions = questions
         self.k = len(self.questions)
         self.score_probabilities = None
 
     def get_score_dispersion(self):
+        """Returns the max_score - min_score (range of the score possibilities."""
         self.get_score_probabilities()
         max_score = None
         min_score = None
@@ -39,25 +47,34 @@ class Student:
         return max_score - min_score
 
     def get_score_probabilities(self):
+        """Returns a list of tuples of (score, probability) for all of the 
+           possible combinations of scores and their probabilities for a given
+           student."""
         if not self.score_probabilities:
             self.score_probabilities = self._exponential_recursion(0, [(0,1)])
         return self.score_probabilities
 
     def _exponential_recursion(self, i, score_prob):
+        # method for getting the score probabilities by doing all possible 
+        # combinations of scores. 
         current_rj = self.questions[i].rj 
         new_score_prob = []
         for score, prob in score_prob:
+            # create new tuples based on each of the previous scores.
             new_score = score + 1.0/current_rj
             incorrect = (score, prob*(1.0-current_rj))
             correct = (new_score, prob*1.0*current_rj)
             new_score_prob.append(incorrect)
             new_score_prob.append(correct)
+        # recurse until we run out of questions
         if i+1 < len(self.questions):
             return self._exponential_recursion(i+1, new_score_prob)
         else:
             return new_score_prob
 
     def get_score_probabilities_approx(self):
+        """Gets an approximation for the score probabilities, unless the 
+           score probabilities are already computed."""
         if self.score_probabilities:
             return self.score_probabilities
         probs = [1] + [0 for x in xrange(self.k-1)]
@@ -78,6 +95,9 @@ class Student:
         return probs
                 
 class Examset:
+    """Class which represents an possible ExamSet to be given to a set of 
+       students. The ExamSet contains a list of students and the possible 
+       questions that will be posed."""
     def __init__(self, student_list, bin_size=None):
         self.students = student_list
         self.entropy = None
@@ -89,27 +109,38 @@ class Examset:
 
 
     def compute_bin_size(self, students):
+        """Computes the bin size to use for computing entropies and 
+           probabilities."""
         max_disp = None
+        min_disp = None
         for student in students:
             disp = student.get_score_dispersion()
             if max_disp == None or disp > max_disp:
                 max_disp = disp
-        self.bin_size = max_disp * 1.0 / 2
+            if min_disp == None or disp < max_disp:
+                min_disp = disp
+        # current heuristic is to take the minimum score dispersion and multiply by 2.5
+        # we would rather have small bin size, then bins which are too large.
+        self.bin_size = min_disp * 2.5
         return self.bin_size
 
     def compute_entropy(self):
+        """Computes the entropy of the examset."""
         if self.entropy:
             return self.entropy
         if not self.bins:
             self.bins = self.build_histogram(self.bin_size)
         entropy = 0
-        for (score, prob_array) in self.bins:
-            avg_prob = prob_array[0] * 1.0 / prob_array[1]
+        for cumulative_prob, count in self.bins.itervalues():
+            avg_prob = cumulative_prob * 1.0 / count
             entropy += get_entropy(avg_prob)
         self.entropy = entropy
         return entropy
 
     def build_histogram(self, bin_size):
+        """Returns a dictionary where keys are the bin, and the values
+           are an array of the total cumulative probability prob and the count
+           so that we have [prob, count]."""
         bins = {}
         # bins that give the total cumulative probability, and count of 
         # the number of occurrences
@@ -125,25 +156,36 @@ class Examset:
         return bins
  
 class ComputeProbabilities:
+    """Class for reading in the question results and creating students."""
     def __init__(self, question_results):
         self.question_results = question_results
+	self.student_results = self._create_students()
 
-    def create_students(self):
+    def _create_students(self):
         students = {}
+
+        # students is a hash that stores a student id as a key and contains 
+        # a StudentResult object
         for question in self.question_results:
             if question.student_id in students:
+                # if studentResult for the id is already in the hash, change it.
                 students[question.student_id].question_results.append(question) 
                 students[question.student_id].num_questions += 1
                 if question.correct:
                     students[question.student_id].num_correct += 1
             else:
+                # if the student id is not in the hash, create a new StudentResult object and add it
                 if students[question.student_id].correct:
                     num_correct = 1
                 else:
                     num_correct = 0
-                    stud_result = StudentResult([question], question.student_id, num_correct, 1)
-                    students[question.student_id] = stud_result
+                stud_result = StudentResult([question], question.student_id, num_correct, 1)
+                students[question.student_id] = stud_result
         return students
+
+    def compute_rj(self):
+        """Method which iteratively computes the probability of a given question begin asked."""
+ 
 
 
 class GreedyAssignment:
