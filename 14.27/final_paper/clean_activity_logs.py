ac26024cb40d00b4b53b55bc7db2d02546f62747
wangjohn <wangjohn@mit.edu>
2012-11-28 04:25:43 -0500

    working on the output of lines of controller data.

--- 14.27/final_paper/clean_activity_logs.py
+++ 14.27/final_paper/clean_activity_logs.py
@@ -17,9 +17,10 @@ class ActivityLog:
         self.session_id = input_lines[10]
         self.impersonated = input_lines[11]
         self.time_from_event = None
+        self.num_views_day_later = None
 
     def convert_to_row(self):
-        return [self.id, self.user_account_id, self.controller, self.action, self.model_id, self.status, self.created_at, self.query_params, self.ip_address, self.next_profile_activity_log_id, self.session_id, self.impersonated, self.time_from_event]
+        return [self.id, self.user_account_id, self.controller, self.action, self.model_id, self.status, self.created_at, self.ip_address, self.next_profile_activity_log_id, self.session_id, self.impersonated, self.time_from_event.seconds + 86400*self.time_from_event.days]
 
     def get_day(self):
         return self.created_at.strftime("%Y-%m-%d")
@@ -38,11 +39,8 @@ def read_in_data(filename):
             counter += 1
             if counter % 100000 == 0:
                 print counter
-            if counter >= 500000:
-                break
     all_logs = sorted(all_logs, key = lambda k : k.created_at)
     return all_logs
-
 
 class FindUserSets:
     def __init__(self, activity_logs):
@@ -63,12 +61,12 @@ class FindUserSets:
         upper_datetime = commit_datetime + datetime.timedelta(days=k)
         lower_index = binary_search_on_created_at(activity_logs, lower_datetime, 0, len(activity_logs)-1)
         upper_index = binary_search_on_created_at(activity_logs, upper_datetime, 0, len(activity_logs)-1)
-        print approx_commit_index, lower_index, upper_index
         before_users = {}
         for i in xrange(lower_index, approx_commit_index, 1):
             current_activity_log = activity_logs[i]
             if current_activity_log.controller == controller:
-                current_activity_log.time_from_event = commit_datetime - current_activity_log.created_at
+                current_activity_log.time_from_event = current_activity_log.created_at.replace(tzinfo=None) - commit_datetime.replace(tzinfo=None) 
+                # get the number of views 1 day later
                 if current_activity_log.user_account_id in before_users:
                     before_users[current_activity_log.user_account_id].append(current_activity_log)
                 else:
@@ -78,39 +76,46 @@ class FindUserSets:
             current_activity_log = activity_logs[i]
             # this must be true in order for them to be be in both before and after
             if current_activity_log.controller == controller and current_activity_log.user_account_id in before_users:
-                current_activity_log.time_from_event = commit_datetime - current_activity_log.created_at
+                current_activity_log.time_from_event = current_activity_log.created_at.replace(tzinfo=None) - commit_datetime.replace(tzinfo=None) 
                 if current_activity_log.user_account_id in both_ba:
                     both_ba[current_activity_log.user_account_id][1].append(current_activity_log)
                 else:
                     both_ba[current_activity_log.user_account_id] = [before_users[current_activity_log.user_account_id], [current_activity_log]]
         return both_ba
 
-    def format_both_ba_into_rows_aggregated(both_ba, output_rows, extra_data, extra_data_names):
+    def format_both_ba_into_rows_aggregated(self, both_ba, output_rows, extra_data, extra_data_names):
         column_names = extra_data_names
         column_names.extend(['user_account_id', 'num_before', 'num_after', 'total'])
         for user_account_id, activity_logs_list in both_ba.iteritems():
+            activity_logs_list[0][0].convert_to_row()
             num_before = len(activity_logs_list[0])
             num_after = len(activity_logs_list[1])
             output_rows.append(extra_data + [user_account_id, num_before, num_after, num_before + num_after])
         return output_rows
 
-    def format_both_ba_into_rows_meancentered(both_ba, output_rows):
-        column_names = ['id', 'user_account_id', 'controller', 'action', 'model_id', 'status', 'created_at', 'query_params', 'ip_address', 'next_profile_activity_log_id', 'session_id', 'impersonated', 'time_from_event']
-        column_names.append('after_commit')
+    def format_both_ba_into_rows_meancentered(self, both_ba, output_rows):
+        #column_names = ['id', 'user_account_id', 'controller', 'action', 'model_id', 'status', 'created_at', 'query_params', 'ip_address', 'next_profile_activity_log_id', 'session_id', 'impersonated', 'time_from_event', 'after_commit', 'num_views_day_later']
+        
         for user_account_id, activity_logs_list in both_ba.iteritems():
-            for activity_log_before in activity_logs_list[0]:
-                output_rows.append(activity_log_before.convert_to_row() + [0])
-            for activity_log_after in activity_logs_list[1]:
-                output_rows.append(activity_log_after.convert_to_row() + [1])
+            for i in xrange(len(activity_logs_list[0])):
+                activity_log_before = activity_logs_list[0][i]
+                j = binary_search_on_created_at(activity_logs_list[0], activity_log_before.created_at + datetime.timedelta(days=1), 0, len(activity_logs_list[0])-1)
+                output_rows.append(activity_log_before.convert_to_row() + [0, j-i])
+            for i in xrange(len(activity_logs_list[1])):
+                activity_log_after = activity_logs_list[1][i]
+                j = binary_search_on_created_at(activity_logs_list[1], activity_log_after.created_at + datetime.timedelta(days=1), 0, len(activity_logs_list[1])-1)
+                output_rows.append(activity_log_after.convert_to_row() + [1, j-i])
         return output_rows
 
 def binary_search_on_created_at(activity_logs, datetime, start, end):
     if end <= start:
         return start
     mid = (start + end)/2
-    if activity_logs[mid].created_at > datetime:
+    created_at = activity_logs[mid].created_at.replace(tzinfo=None)
+    datetime = datetime.replace(tzinfo=None)
+    if created_at > datetime:
         return binary_search_on_created_at(activity_logs, datetime, start, mid-1)
-    elif activity_logs[mid].created_at < datetime:
+    elif created_at < datetime:
         return binary_search_on_created_at(activity_logs, datetime, mid+1, end)
     else:
         return mid
@@ -120,7 +125,6 @@ if __name__ == '__main__':
     print 'all loaded'
     test_datetime = datetime.datetime(2012, 8, 14)
     fus = FindUserSets(all_logs)
-    cap = fus.find_controllers()
     print cap
     print len(cap)
     #b = fus.find_users(all_logs, "my_panjiva", "async_activity_feed", test_datetime, 10)
