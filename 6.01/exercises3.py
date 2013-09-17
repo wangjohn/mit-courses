3ba264f818813978d942ceb6c65781780488e32f
wangjohn <wangjohn@mit.edu>
2013-09-17 13:11:25 -0500

    exercises

---
+++ 6.01/exercises3.py
@@ -0,0 +1,46 @@
+from lib601.search import search
+
+class FGWCSearchNode(SearchNode):
+    def __init__(self, state, parent):
+        SearchNode.__init__(self, state)
+        self.parent = parent
+
+    def getChildren(self):
+        objectsWithFarmer = self._objectsWithFarmer(self.state)
+        potential_states = []
+        potential_states.append(self._changeTuple(self.state, [0]))
+        for i in xrange(len(objectsWithFarmer)):
+            potential_states.append(self._changeTuple(self.state, [0, i]))
+
+        return [FGWCSearchNode(state, self) for state in potential_states if self._isLegal(state)]
+
+    def _changeTuple(self, state, states_to_change):
+        list_result = []
+        for i in xrange(len(state)):
+            if i in states_to_change:
+                if state[i] == 'R':
+                    list_result.append('L')
+                else:
+                    list_result.append('R')
+            else:
+                list_result.append(state[i])
+        return (list_result[0], list_result[1], list_result[2], list_result[3])
+
+    def _objectsWithFarmer(self, state):
+        objects = []
+        for i in xrange(len(state)): 
+            if state[i] == state[0] and i > 0:
+                objects.append(i)
+        return objects
+
+    def _isLegal(self, state):
+        if state[1] == state[3] or state[1] == state[2]:
+            return False
+        return True
+
+def goalTest(s):
+    return s == ('R', 'R', 'R', 'R')
+
+if __name__ == '__main__':
+    ans = [i.state for i in FGWCSearchNode(('R','R','L','L'),None).getChildren()]
+    print ans
