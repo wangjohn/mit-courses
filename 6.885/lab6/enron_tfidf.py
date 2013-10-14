5c775bde28db93b821cd7e78b593d3b670da7ec8
wangjohn <wangjohn@mit.edu>
2013-10-14 04:46:24 +0000

    Finishing up tfidf computation in spark.

--- 6.885/lab6/enron_tfidf.py
+++ 6.885/lab6/enron_tfidf.py
@@ -12,20 +12,48 @@ allowed_senders = set([
     'rebecca.mark@enron.com'
     ])
 
+def get_terms(text)
+    return text.lower().split('[\s,.]')
+
+def compute_sender_term_count(sender, text):
+    terms = get_terms(text)
+    for term in terms:
+        yield (sender, term), 1.0
+
+def compute_sender_term_frequency(term_count, total):
+    return float(term_count) / total
+
+def get_id_term_pair(doc_id, text):
+    terms = get_terms(text)
+    for term in terms:
+        yield doc_id, term
+
+def compute_idf(term, count):
+    return term, math.log(count, 10)
 
 print 'loading'
 sc = SparkContext("local", "Simple App")
 # Replace `lay-k.json` with `*.json` to get a whole lot more data.
 lay = sc.textFile('s3n://AKIAJFDTPC4XX2LVETGA:<AWS KEY FROM PIAZZA>@6885public/enron/lay-k.json')
+lay = lay.map(lambda x: json.loads(x))
 
-filtered_lay = lay.map(lambda x: json.loads(x)).filter(lambda x: x['sender'] in allowed_senders)
+sender_term_counts = lay.filter(lambda x: x['sender'] in allowed_senders)
+                        .map(lambda x: compute_sender_term_count(x['sender'], x['text']))
+                        .reduceByKey(add)
+sender_totals = sender_term_counts.map(lambda x: (x[0][0], x[1])).reduceByKey(add).cache()
+sender_term_frequencies = sender_term_counts.map(lambda x: (x[0], compute_sender_term_frequency(x[1], sender_totals[x[0][0]])))
 
-terms = lay.flatMap(lambda x: [{ 'term': term, 'mid': x['mid'], 'sender': x['sender'] } for term in x['text'].lower().split('[\s,.]')]).cache()
+document_counts = lay.map(lambda x: get_id_term_pair(x['mid'], x['text'])).distinct().map(lambda x: (x[1], 1.0)).reduceByKey(add)
+document_idfs = document_counts.map(lambda x: compute_idf(term, count))
 
-sender_term_counts = terms.map(lambda x: { 'sender': x['sender'], 'term': x['term'] }).reduceByKey(add)
-sender_sums = sender_term_counts.map(lambda x: (x[0]['sender'], x[1])).reduceByKey(add)
-print sender_sums.collect()
+tfidfs = sender_term_frequencies.map(lambda x: (x[0], x[1]*document_idfs[x[0][1]]))
+                                .groupBy(lambda x: x[0][0])
+                                .map(lambda x: (x[0], sorted(x[1], reverse=True)[:10]))
 
-doc_frequencies = terms.distinct().map(lambda x: x['term']).countByKey().map(lambda x: (x[0], math.log(x[1], 10)))
-print doc_frequencies.collect()
+for sender, results in tfidfs.collect():
+    print ''
+    print '------------------------'
+    print sender
+    for term in results
+        print term
 
